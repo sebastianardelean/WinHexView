9eca17eb8bddf28958b9567dbe5cf5c8a5427e40
23ars <ardeleanasm@gmail.com>
2016-04-30 16:34:41 +0300

    Implement help #5

--- hexview/FileReader.cpp
+++ hexview/FileReader.cpp
@@ -6,7 +6,7 @@ FileReader::FileReader(std::wstring filePath)
 {
 	gFilePath = filePath;
 	gFileHandle = nullptr;
-	
+
 }
 
 
@@ -15,7 +15,7 @@ FileReader::~FileReader()
 	if (gFileHandle != nullptr){
 		(void)CloseHandle(gFileHandle);
 	}
-	
+
 }
 
 DWORD FileReader::Read(HANDLE hFile, void *buffer, INT64 count)throw (std::exception)
@@ -36,10 +36,11 @@ DWORD FileReader::Read(HANDLE hFile, void *buffer, INT64 count)throw (std::excep
 
 void *FileReader::LoadFileContent(DWORD *bufferSize) throw (std::exception)
 {
-	
+
 	LARGE_INTEGER file_size;
 	UINT64 size;
 	memset(&file_size, 0, sizeof(LARGE_INTEGER));//set default to 0
+
 	gFileHandle = CreateFile(gFilePath.c_str(),//filename, in UNICODE
 		GENERIC_READ,// open only for read,
 		FILE_SHARE_READ,//it can be shared with other process for read purpose
@@ -48,18 +49,21 @@ void *FileReader::LoadFileContent(DWORD *bufferSize) throw (std::exception)
 		FILE_ATTRIBUTE_READONLY,// file is readonly
 		NULL//hTemplateFile is null
 		);
+
+
+
 	if (gFileHandle == INVALID_HANDLE_VALUE){
 		gFileHandle = nullptr;
 		throw new FileManagementException("Could not open file!");
 		return nullptr;
 	}
-	
+
 	if (GetFileSizeEx(gFileHandle, &file_size) == FALSE){
 		memset(&file_size, 0, sizeof(LARGE_INTEGER));
 		throw new FileManagementException("Could not get file size!");
 		return nullptr;
 	}
-	
+
 	if (file_size.QuadPart % 8 == 0){
 		size = file_size.QuadPart;
 	}
@@ -70,11 +74,11 @@ void *FileReader::LoadFileContent(DWORD *bufferSize) throw (std::exception)
 	memset(buffer, 0, size);
 	try
 	{
-		*bufferSize=Read(gFileHandle, &buffer[0], size);//don't care about how much it reads for now
+		*bufferSize = Read(gFileHandle, &buffer[0], size);//don't care about how much it reads for now
 		if (*bufferSize % 8 != 0){
 			*bufferSize = *bufferSize + (8 - *bufferSize % 8);
 		}
-		
+
 	}
 	catch (std::exception &e){
 		e.what();
--- hexview/hexview.cpp
+++ hexview/hexview.cpp
@@ -15,8 +15,8 @@ int _tmain(int argc, _TCHAR* argv[])
 	}
 	void *buffer;
 	DWORD buffer_size;
-
-	std::unique_ptr<FileReader> reader(new FileReader(argv[1]));
+	std::wstring file_path = L"";
+	
 	std::shared_ptr<Display>display = std::make_shared<Display>();
 	std::unique_ptr<Command> command(new Command(display));
 	
@@ -28,7 +28,12 @@ int _tmain(int argc, _TCHAR* argv[])
 		if (wcscmp(argv[arg_index], L"/A") == 0){
 			command->SetExecuteMethod(&Display::AsciiPrint);
 		}
+		if (wcscmp(argv[arg_index], L"/F") == 0){
+			file_path = argv[arg_index + 1];
+		}
 	}
+
+	std::unique_ptr<FileReader> reader(new FileReader(file_path));
 	try{
 		if ((buffer = reader->LoadFileContent(&buffer_size)) != nullptr){
 			display->Configure((byte*)buffer, buffer_size);
@@ -47,5 +52,8 @@ int _tmain(int argc, _TCHAR* argv[])
 
 void print_help(_TCHAR *argv)
 {
-	std::wcout << argv;
+	std::wcout << argv << L" [Options]\n";
+	std::wcout << L"/F\t\tinput file[if not specified, stdin is used]\n";
+	std::wcout << L"/C\t\tdisplay ASCII characters\n";
+
 }
